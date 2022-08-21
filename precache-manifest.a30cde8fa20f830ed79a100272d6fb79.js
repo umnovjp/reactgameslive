25e699bd5acbbd8f0dade2f62aa2c41e3b015879
umnovjp <umnovjp@yahoo.com>
2022-08-20 20:31:48 -0700

    Updates

---
+++ precache-manifest.a30cde8fa20f830ed79a100272d6fb79.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "8ab349cdfe0fe6c0c503",
+    "url": "/reactgamelive/static/js/main.bc85c7be.chunk.js"
+  },
+  {
+    "revision": "7f1ff467a926a8f37892",
+    "url": "/reactgamelive/static/js/runtime~main.6f442f4b.js"
+  },
+  {
+    "revision": "dcdd53a549fcb4c76be8",
+    "url": "/reactgamelive/static/css/2.b8fb7e5a.chunk.css"
+  },
+  {
+    "revision": "dcdd53a549fcb4c76be8",
+    "url": "/reactgamelive/static/js/2.f07521a8.chunk.js"
+  },
+  {
+    "revision": "fcadf091a1019d9fc13f10b09d9065e2",
+    "url": "/reactgamelive/index.html"
+  }
+];
