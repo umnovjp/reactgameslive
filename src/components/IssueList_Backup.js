557685ceb3a6d958b8c7e07e5258d07c440eb2a5
umnovjp <umnovjp@yahoo.com>
2022-08-23 22:09:48 -0700

    taking to the point when games stats will be displayed

--- src/components/IssueList_Backup.js
+++ src/components/IssueList_Backup.js
@@ -6,9 +6,8 @@ const IssueList = ({ issues }) => {
   // We create a list of rendered IssueItems by using the map method on `issues`
   const renderedList = issues.map((issue) => {
     return <IssueItem key={issue.id} issue={issue} />;
-   
   });
-  console.log(issues);
+
   // Here we return our array of IssueItems wrapped inside a parent div
   return <div className="ui relaxed divided list">{renderedList}</div>;
 };
