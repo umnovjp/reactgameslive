ecd46b31fe9703abcbbd1b98d73906ca2868b539
umnovjp <umnovjp@yahoo.com>
2022-09-08 20:07:52 -0700

    finally succeded to convert results from object to an array

--- src/components/PlayerList.js
+++ src/components/PlayerList.js
@@ -2,17 +2,17 @@ import React from 'react';
 // console.log('I am in ResultList', typeof props.results.players, Object.values(props.results.players), props.results.players)
 // In our return method, we use the map method to return a new array of `li` and `img` elements that are specific to each search result
 
-function ResultList(props) {
+function PlayerList(props) {
  // const rosterString = JSON.stringify(props.results.players);
  // const roster = rosterString.split('fullName')
  // console.log(rosterString)
   return (
     <ul className="list-group">
-    {/* {const roster1 = props.results.players;} 
-    console.log('I am in ResultList', Object.values(props.results)[5])
-    */}
-      topbar result list
-      {
+    {/* {const roster1 = props.results.players;
+    const roster2 = Object.values(roster1)} */}
+      topbar player list
+      {console.log('I am in PlayerList', props.results)
+
         // {props.results}
       // props.results.map((result) => (
       //   <li className="list-group-item">
@@ -29,4 +29,4 @@ function ResultList(props) {
   );
 }
 
-export default ResultList;
+export default PlayerList;
--- src/components/SearchResultContainer.js
+++ src/components/SearchResultContainer.js
@@ -3,6 +3,7 @@ import { useState, useEffect } from 'react';
 import ResultList from './ResultList';
 // Import our search method
 import search from '../utils/API';
+import PlayerList from './PlayerList';
 const rosterArray = [];
 const SearchResultContainer = () => {
   // Declare a new state variable, "results"
@@ -11,29 +12,31 @@ const SearchResultContainer = () => {
   // Method to get search results and set state
   const searchGiphy = async (query) => {
     const response = await search(query);
-    setResults(response.data.gameData); // original was response.data.data; we pass only gameData to resultList
-    console.log(response.data.gameData.players);
-    var fullRoster = response.data.gameData.players;
+    //setResults(response.data.liveData.boxscore.teams.home.players); // original was response.data.data; we pass only gameData to resultList
+    console.log(response.data.liveData.boxscore.teams.home.players);
+    var fullRoster = response.data.liveData.boxscore.teams.home.players;
     var rosterArray = Object.values(fullRoster);
     console.log(rosterArray);
-    for (let i = 0; i < rosterArray.length; i++) {
-    const fullName1 = rosterArray[i].fullName
-      console.log(fullName1, rosterArray[i].primaryPosition.code, rosterArray[i].shootsCatches, rosterArray[i].primaryNumber, rosterArray[i].currentTeam)};//rosterArray[i].split('fullName]')
-    console.log(results) // searchResultContainer does not know what results are, but resultList knows
-      };
+    setResults(rosterArray)
+    // for (let i = 0; i < rosterArray.length; i++) {
+    // const fullName1 = rosterArray[i].fullName
+    //   console.log(fullName1, rosterArray[i].primaryPosition.code, rosterArray[i].shootsCatches, rosterArray[i].primaryNumber, rosterArray[i].currentTeam)};//rosterArray[i].split('fullName]')
+    //   console.log(results + 'will be undefined') // searchResultContainer does not know what results are, but resultList knows
+  };
   
   // We want to run this method when the component first loads so that we have images of kittens to display
   // The second argument is the dependency array. This means that this method will only run when the component first loads
   useEffect(() => {
     searchGiphy('2021021133');
   }, []);
-  console.log('5', rosterArray[5])
+  console.log('5', rosterArray[2])
   return (
     <div>
       {/* Pass our results to the ResultsList component to map over 
       <ResultList results={results} />*/}
       <ResultList results={results} />
-      <li>{rosterArray[5]}</li>
+     <PlayerList results = {results} />
+      <li>list in search</li>
     </div>
   );
   
